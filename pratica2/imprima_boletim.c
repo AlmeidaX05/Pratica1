e266a0fb0a3e4d8006abb820d36200ce1c54bb12
AlmeidaX05 <fcoalmeida2005@gmail.com>
2024-08-21 13:43:55 +0000

    anfbsdj

---
+++ pratica2/imprima_boletim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("%c\t \nMatricula         A1       A2       Media ");  
+  printf("%i\t \n20210010          8.5      5.0      6.4 ");
+  printf("%i\t \n20215053          4.7     10.0      7.9 ");
+  printf("%i\t \n20220027          5.6      8.3      7.2 ");
+  printf("%i\t \n20220103          9.2      1.5      4.6 ");  
+  return 0;  
+}
---
+++ pratica2/imprima_seu_nome.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+int main() {
+  printf("\nFrancisco Almeida\n");
+  return 0;
+}
